ba4e8b513c04916bb96c48d8eab8630e7ace7957
Declan Harrington <declanharrington123@gmail.com>
2026-07-15 19:51:43 -0400

    Added tab functionality + close tab - will add ui for open/close next

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,54 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <Qsci/qsciscintilla.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_tab = new QTabWidget(this);
+    m_tab -> setTabsClosable(true);
+    m_tab -> setMovable(true);
+    connect(m_tab, &QTabWidget::tabCloseRequested, this, &MainWindow::closeTab);
+    setCentralWidget(m_tab);
+
+    newEditorTab("Untitled");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+QsciScintilla* MainWindow::currentEditor() const {
+    return qobject_cast<QsciScintilla*>(m_tab -> currentWidget());
+}
+
+QsciScintilla* MainWindow::newEditorTab(const QString& title) {
+    auto* editor = new QsciScintilla(m_tab);
+    editor -> setMarginType(0, QsciScintilla::NumberMargin);
+    editor -> setMarginWidth(0, "0000");
+    editor -> setAutoIndent(true);
+    editor -> setTabWidth(4);
+
+    connect(editor, &QsciScintilla::textChanged, this, [this, editor]() {
+        m_modified[editor] = true;
+    });
+
+    m_tab -> addTab(editor, title);
+    m_tab -> setCurrentWidget(editor);
+    return editor;
+}
+
+void MainWindow::closeTab(int index) {
+    // sets editor to tab we want to close
+    auto* editor = qobject_cast<QsciScintilla*>(m_tab -> widget(index));
+
+    m_filePaths.remove(editor);
+    m_modified.remove(editor);
+    m_tab -> removeTab(index);
+    editor -> deleteLater(); // learn more about this function
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+class QsciScintilla;
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -19,5 +21,20 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QsciScintilla* m_editor = nullptr;
+    QString m_currentFilePath;
+
+    QTabWidget* m_tab = nullptr;
+    // Qtab widget knows about widgets and tab titles, not about 
+    // which file it is or if its been edited, we have to track that 
+    // with two sepserate maps
+    QMap<QsciScintilla*, QString> m_filePaths;
+    QMap<QsciScintilla*, bool> m_modified;
+
+    QsciScintilla* currentEditor() const;
+    QsciScintilla* newEditorTab(const QString& title);
+    void applyLexerFor(QsciScintilla* editor, const QString& filePath);
+    void applyLexerForCurrentFile();
+    void closeTab(int index);
 };
 #endif // MAINWINDOW_H
