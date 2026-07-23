f764a27dcaaf8706a98df7bd908b4f8a3ad4191b
Declan Harrington <declanharrington123@gmail.com>
2026-07-22 23:33:40 -0400

    Cleanedup close tab funtionality - added a + tab to create new window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,6 @@
 #include "mainwindow.h"
+#include <QToolButton>
+#include <QMessageBox>
 #include "./ui_mainwindow.h"
 #include <Qsci/qsciscintilla.h>
 
@@ -13,6 +15,15 @@ MainWindow::MainWindow(QWidget *parent)
     m_tab -> setMovable(true);
     connect(m_tab, &QTabWidget::tabCloseRequested, this, &MainWindow::closeTab);
     setCentralWidget(m_tab);
+
+    auto* newTabButton = new QToolButton(this);
+    newTabButton->setText("+");
+    newTabButton->setToolTip("New Tab");
+
+    connect(newTabButton, &QToolButton::clicked, this, [this]() {
+        newEditorTab("untitled");
+    });
+    m_tab->setCornerWidget(newTabButton, Qt::TopRightCorner);
 
     newEditorTab("Untitled");
 }
@@ -45,9 +56,34 @@ QsciScintilla* MainWindow::newEditorTab(const QString& title) {
 void MainWindow::closeTab(int index) {
     // sets editor to tab we want to close
     auto* editor = qobject_cast<QsciScintilla*>(m_tab -> widget(index));
+    if (!editor) {
+        return;
+    }
+    
+    QMessageBox::StandardButton result = QMessageBox::Discard;
 
+    if (m_modified.value(editor, false)) {
+        auto result = QMessageBox::question(
+            this, "Unsaved changes",
+            "This file has unsaved changes. Save before closing?",
+            QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel
+        );
+    }
+
+    // if user doesn't want to close the tab - abort beofre removing below
+    if (result == QMessageBox::Cancel) {
+        return;
+    }
+
+    if (result == QMessageBox::Save) {
+        // TODO: create a saveEditor(editor) later 
+    }
     m_filePaths.remove(editor);
     m_modified.remove(editor);
+
+    if (m_tab->count() <= 1) {
+        newEditorTab("untitled"); // replace the last tab with a fresh blank one, might be some form of home tab in the future
+    }
     m_tab -> removeTab(index);
     editor -> deleteLater(); // learn more about this function
 }
